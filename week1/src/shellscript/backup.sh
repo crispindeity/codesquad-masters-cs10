aa77e1e7bb3ba8b25c7e9e26a1f01c60a0158e8b
geombong <shoy1415@naver.com>
2022-01-08 09:31:26 +0900

    쉘 스크립트

--- week1/src/shellscript/backup.sh
+++ week1/src/shellscript/backup.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 check_backup_dir() {
-	if ! [ -d backup ]
-	then
+  if ! [ -d backup ]
+  then
       mkdir backup
-	fi
+  fi
 }
 
 is_empty_file() {
-	if [ -e *.cs ]
-	then
+  if [ -e *.cs ]
+  then
       cp *.cs ../backup
-	else
+  else
       echo "$i" is empty
-	fi
+  fi
 }
 
 file_backup() {
-	today=$(date "+%Y%m%d")
-	backup_file_name="backup_$today"
-	
-	zip "$backup_file_name".zip backup/*
+  today=$(date "+%Y%m%d")
+  backup_file_name="backup_$today"
+
+  zip "$backup_file_name".zip backup/*
 }
 
 backup_file_del() {
-	del_day=30
-	
-	sudo find . -type f -name '*.cs' -mtime +$del_day  -exec bash -c "rm -f {}" \; 2> /dev/null
+  del_day=30
+
+  sudo find . -type f -name '*.cs' -mtime +$del_day  -exec bash -c "rm -f {}" \; 2> /dev/null
 }
 
 transfer_file() {
-	scp "$backup_file_name".zip geombong@192.xxx.xx.x:~/backup
+  scp "$backup_file_name".zip geombong@192.xxx.xx.x:~/backup
 }
 
 dir_lists=$(ls -d day*)

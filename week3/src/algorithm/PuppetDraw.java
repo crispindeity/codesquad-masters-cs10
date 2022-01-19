8e25ef690c2c67c81f297a940b5d4cdda69c323d
geombong <shoy1415@naver.com>
2022-01-19 14:34:27 +0900

    프로그래머스_카카오_인형뽑기

--- week3/src/algorithm/PuppetDraw.java
+++ week3/src/algorithm/PuppetDraw.java
@@ -57,7 +57,7 @@ class PuppetDrawTest {
     void puppetDrawTest2() {
         PuppetDraw puppetDraw = new PuppetDraw();
 
-        int[][] testBoard = { // 4 3 1 1 3 2 2 0 4
+        int[][] testBoard = {
                 {0, 0, 0, 0, 0},
                 {0, 0, 1, 0, 3},
                 {0, 2, 5, 0, 1},
